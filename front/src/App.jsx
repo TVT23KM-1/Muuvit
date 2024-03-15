0842f4bd94c2f3eea5d8a2ed1e36bda146f21019
Rebecca Soisenniemi <t2sokr00@students.oamk.fi>
2024-03-15 16:16:39 +0200

    added few components

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -7,6 +7,9 @@ import Home from './components/Home/Home';
 import Search from './components/Search/Search';
 import Community from './components/Community/Community';
 import Login from './components/Login/Login';
+import ScreenError from './components/ScreenError/ScreenError';
+import Navi from './components/Navi/Navi';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -15,42 +18,19 @@ const App = () => {
     <Router>
       <ThemeProvider>
         <div className={`body ${theme}`}>
-          <div id="screen-error">
-            <h1>Muuvi</h1>
-            <h2>Liian pieni ruutu.</h2>
-            <h3>Sivustoa ei voida avata.</h3>
-          </div>
+          <ScreenError />
+          <Navi />
 
-          <div id="container">
-            <div id="colorbar">
-              <div className="header-container">
-                <h1>Muuvi</h1>
-                <ul className="navi">
-                  <li><Link to="/">Etusivu</Link></li>
-                  <li><Link to="/search">Muuvihaku</Link></li>
-                  <li><Link to="/community">Yhteisö</Link></li>
-                  <li><Link to="/login">Kirjautuminen</Link></li>
-                </ul>
-              </div>
-            </div>
-          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/community" element={<Community />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
 
-          <div id="content">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/search" element={<Search />} />
-              <Route path="/community" element={<Community />} />
-              <Route path="/login" element={<Login />} />
-            </Routes>
-
-            <div id="footer">
-              <span className="gh-mark"></span> <a href="https://github.com/TVT23KMO-1/2024-q2-elokuvasovellus">Github repositio</a><br/>
-              <p>© OAMK Tietotekniikan opiskelijat, 2024</p>
-              <button onClick={toggleTheme}>Vaihda teemaa</button>
-              <p>Tämä on {theme === 'light' ? 'vaalea' : 'tumma'} teema.</p>
-            </div>
-          </div>
+          <Footer toggleTheme={toggleTheme} theme={theme} />
         </div>
+
       </ThemeProvider>
     </Router>
   );
