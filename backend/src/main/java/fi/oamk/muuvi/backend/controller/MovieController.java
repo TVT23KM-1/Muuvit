5463b6c8aabd86ea3b3a53653b22ad8cd26d36bc
Juho Hietala <juhohietala90@gmail.com>
2024-03-22 12:18:42 +0200

    Elokuva haku toimii. application.yml joka sisaltaa api_keyn, jatetty pois.

--- backend/src/main/java/fi/oamk/muuvi/backend/controller/MovieController.java
+++ backend/src/main/java/fi/oamk/muuvi/backend/controller/MovieController.java
@@ -2,8 +2,26 @@ package fi.oamk.muuvi.backend.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import fi.oamk.muuvi.backend.Shemas.MovieResult;
+import fi.oamk.muuvi.backend.services.MovieService;
+import okhttp3.Response;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 @RestController
 @RequestMapping("/movie")
 public class MovieController {
-
+    private MovieService movieService;
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+    
+    @GetMapping("/search")
+    public ResponseEntity<MovieResult> getMethodName() {
+        return movieService.search();
+    }
+    
 }
--- backend/src/main/java/fi/oamk/muuvi/backend/services/MovieService.java
+++ backend/src/main/java/fi/oamk/muuvi/backend/services/MovieService.java
@@ -1,5 +1,51 @@
 package fi.oamk.muuvi.backend.services;
 
-public class MovieService {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestClientException;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import fi.oamk.muuvi.backend.Shemas.MovieResult;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+@Service
+public class MovieService {
+    @Value("${tmdb.api_key}")
+    private String api_key;
+
+    public String getApiKey() {
+        return api_key;
+    }
+
+    public ResponseEntity<MovieResult> search() {
+        OkHttpClient client = new OkHttpClient();
+        String URL = String.format("https://api.themoviedb.org/3/discover/movie?api_key=%s&with_genres=%s", this.getApiKey(), 27);
+
+        Request request = new Request.Builder()
+        .url(URL)
+        .get()
+        .addHeader("accept", "application/json")
+        .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                return ResponseEntity.status(response.code()).body(null);
+            }
+
+            String responseBody = response.body().string();
+
+            ObjectMapper mapper = new ObjectMapper();
+            MovieResult movieResult = mapper.readValue(responseBody, MovieResult.class);
+
+            return ResponseEntity.ok(movieResult);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
 }
