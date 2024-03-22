ef4706783f50a1e048fb7524ff85846b55d8a9de
Rebecca Soisenniemi <t2sokr00@students.oamk.fi>
2024-03-22 12:25:53 +0200

    Security conf

--- backend/src/main/java/fi/oamk/muuvi/backend/SecurityConfig.java
+++ backend/src/main/java/fi/oamk/muuvi/backend/SecurityConfig.java
@@ -1,25 +1,26 @@
-//package fi.oamk.muuvi.backend;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.Customizer;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
 
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//        // for every http request
-//            .authorizeHttpRequests((authz) -> authz
-//            // allow any user to access openapi documentation
-//            .requestMatchers("/v3/api-docs/**", "/v3/api-docs.yaml", "/swagger-ui/**", "/swagger-ui.html", "/event/theatreAreas").permitAll()
-//            // any other endpoint requires authenticated user
-//            .anyRequest().authenticated()
-//        ).httpBasic(Customizer.withDefaults());
-//        return http.build();
-//    }
-//}
+package fi.oamk.muuvi.backend;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+        // for every http request
+            .authorizeHttpRequests((authz) -> authz
+            // allow any user to access openapi documentation
+            .requestMatchers("/v3/api-docs/**", "/v3/api-docs.yaml", "/swagger-ui/**", "/swagger-ui.html", "/event/theatreAreas").permitAll()
+            // any other endpoint requires authenticated user
+            .anyRequest().authenticated()
+        ).httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
