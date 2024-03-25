6acf1c846d98ab04121aaebd050089237d3620f8
Mikko Kujala <m.kujala@live.com>
2024-03-25 17:06:26 +0200

    kuskin vaihto

--- backend/src/main/java/fi/oamk/muuvi/backend/models/Group.java
+++ backend/src/main/java/fi/oamk/muuvi/backend/models/Group.java
@@ -1,5 +1,17 @@
 package fi.oamk.muuvi.backend.models;
 
-public class Group {
+import jakarta.persistence.*;
 
+
+@Entity
+@Table(name = "groups_")
+public class Group {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long groupId;
+
+    private String groupName;
+    private String groupDescription;
+
+    // Foreign key user_id
 }
