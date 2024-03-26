094bf7c66a799144aa95ef919debd3f7cf0d4bfb
Mikko Kujala <m.kujala@live.com>
2024-03-26 16:49:40 +0200

    jotain räpellystä

--- backend/src/main/java/fi/oamk/muuvi/backend/repositories/UserRepository.java
+++ backend/src/main/java/fi/oamk/muuvi/backend/repositories/UserRepository.java
@@ -1,5 +1,8 @@
 package fi.oamk.muuvi.backend.repositories;
 
-public class UserRepository {
+import fi.oamk.muuvi.backend.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
