498c7fd0db5355ab736f01d63278b561fbf211ad
Juho Hietala <juhohietala90@gmail.com>
2024-03-27 15:36:57 +0200

    Backend_Movie mergetty mainin kanssa. Modeleita muokattu ja palvelin käynnistyy. Modeleiden osalta testaamatta

--- backend/src/main/java/fi/oamk/muuvi/backend/repositories/EventRepository.java
+++ backend/src/main/java/fi/oamk/muuvi/backend/repositories/EventRepository.java
@@ -1,5 +1,9 @@
 package fi.oamk.muuvi.backend.repositories;
 
-public class EventRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import fi.oamk.muuvi.backend.models.Event;
+
+public interface EventRepository extends JpaRepository<Event, Long>{
     
 }
