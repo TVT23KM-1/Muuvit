23c317b6c95d2bb8e916e87a06bc2bb7d91ceaf1
Mikko Kujala <m.kujala@live.com>
2024-04-02 14:04:27 +0300

    Headeria korjailtu
    Headerin teksti ei skaalautunut nätisti näytön koon mukaan.
    Nyt sen pitäisi skaalautua. Poistin tekstin headerista, ja muutin
    headerin olemaan taustakuvana, ja normaalin <h1> tekstin sen päälle.

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { ThemeProvider } from './ThemeProvider.jsx';
 import './index.css';
 import './media.css';
 import { BrowserRouter } from 'react-router-dom';
