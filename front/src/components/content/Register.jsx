aec075e30c1f5db5e2c75544d69eeaf6197f4ef4
taunojokinen <128710496+taunojokinen@users.noreply.github.com>
2024-04-04 09:56:13 +0300

    register kesken

--- front/src/components/content/Register.jsx
+++ front/src/components/content/Register.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
+import axios from 'axios'
 
 const Register = () => {
-  const [showRegisterForm, setShowRegisterForm] = useState(false);
+  const [showRegisterForm, setShowRegisterForm] = useState(false)
+  const [credentials, setCredentials] = useState({})
 
   const openRegisterForm = () => {
     setShowRegisterForm(true);
@@ -11,9 +13,23 @@ const Register = () => {
     setShowRegisterForm(false);
   }
 
-  const register = () => {
-    // logiikka
-    alert('click');
+  const register = () => {    
+    alert( `${credentials.userName} ${credentials.password}` )
+    axios.post('/createUser', credentials)
+    .then(function (response) {
+      console.log(response)
+    })
+    .catch(function(error) {
+      console.log(error)
+    })
+  }
+
+  const handleUsernameChange = (event) => {
+    setCredentials({...credentials, userName: event.target.value})
+  }
+
+  const handlePasswordChange = (event) => {
+    setCredentials({...credentials, password: event.target.value})
   }
 
   return (
@@ -29,8 +45,8 @@ const Register = () => {
         <div>
           <p class="info">Täytä molemmat kentät. Nimimerkki on samalla kirjautumistunnuksesi. Se on uniikki, eikä voi olla sama toisella käyttäjällä.</p>
 
-          <p>Nimimerkki: <input className="field" type="text" placeholder="Valitse nimimerkki" /></p>
-          <p>Salasana: <input className="field" type="text" placeholder="Valitse salasana" /></p>
+          <p>Nimimerkki: <input className="field" onChange={handleUsernameChange} type="text" placeholder="Valitse nimimerkki" /></p>
+          <p>Salasana: <input className="field" onChange={handlePasswordChange} type="text" placeholder="Valitse salasana" /></p>
           <button class="green" onClick={register}>Rekisteröidy</button> 
           <button onClick={closeRegisterForm}>Peruuta</button>
 
