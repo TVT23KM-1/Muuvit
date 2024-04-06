215508203a502ad6fd6e6b1d96d0258685c50946
Mikko Kujala <m.kujala@live.com>
2024-04-06 14:10:27 +0300

    joo kesken on, ja merge tarvitaan toisesta haarasta

--- backend/src/main/java/fi/oamk/muuvi/backend/controller/GroupController.java
+++ backend/src/main/java/fi/oamk/muuvi/backend/controller/GroupController.java
@@ -1,9 +1,25 @@
 package fi.oamk.muuvi.backend.controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import fi.oamk.muuvi.backend.Shemas.NewGroup;
+import fi.oamk.muuvi.backend.services.GroupService;
+import fi.oamk.muuvi.backend.services.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/group")
+@CrossOrigin(origins = "*")
 public class GroupController {
+
+    private GroupService groupService;
+    public GroupController(GroupService groupService) {
+        this.groupService = groupService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<String> createGroup(@RequestBody NewGroup group, @RequestHeader("Authorization") String token) {
+        this.groupService.createGroup(group);
+        return new ResponseEntity<String>("Group created", org.springframework.http.HttpStatus.OK);
+    }
 
 }
--- backend/src/main/java/fi/oamk/muuvi/backend/services/GroupService.java
+++ backend/src/main/java/fi/oamk/muuvi/backend/services/GroupService.java
@@ -1,5 +1,23 @@
 package fi.oamk.muuvi.backend.services;
 
+import fi.oamk.muuvi.backend.Shemas.NewGroup;
+import fi.oamk.muuvi.backend.models.Group;
+import fi.oamk.muuvi.backend.repositories.GroupRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class GroupService {
 
+    GroupRepository repo;
+
+    public GroupService(GroupRepository repo) {
+        this.repo = repo;
+    }
+
+    public void createGroup(NewGroup group) {
+        Group newGroup = new Group();
+        newGroup.setGroupName(group.getGroupName());
+        newGroup.setGroupDescription(group.getDescription());
+        repo.save(newGroup);
+    }
 }
