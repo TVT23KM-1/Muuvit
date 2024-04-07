8ac678833e31cc3ec4b2cbf6540658f59b449064
taunojokinen <128710496+taunojokinen@users.noreply.github.com>
2024-04-07 13:00:25 +0300

    service lisätty

--- backend/src/main/java/fi/oamk/muuvi/backend/services/ReviewService.java
+++ backend/src/main/java/fi/oamk/muuvi/backend/services/ReviewService.java
@@ -1,5 +1,39 @@
 package fi.oamk.muuvi.backend.services;
 
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import fi.oamk.muuvi.backend.models.Review;
+import fi.oamk.muuvi.backend.repositories.ReviewRepository;
+
+import io.micrometer.core.ipc.http.HttpSender.Response;
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
 public class ReviewService {
+    ReviewRepository repo;
+
+    ReviewService(ReviewRepository repo) {
+        this.repo = repo;
+    }
+
+    public ResponseEntity<String> newReview(Integer movieId, Integer stars, String description) {
+        Review newreview = new Review();
+        try {
+            newreview.setMovieId(movieId);
+            newreview.setStars(stars);
+            newreview.setDescription(description);
+            this.repo.save(newreview);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return ResponseEntity.badRequest().body("Invalid review");
+        }
+        return ResponseEntity.ok().body("Review created");
+        
+    }
 
 }
