b49d94b911e57b06b9368a2bc9518e79e1f1ac83
Juho Hietala <juhohietala90@gmail.com>
2024-04-08 11:06:51 +0300

    Testausohjelmisto alustettu Node ymparistoon. Login testit hyvalla mallilla.

---
+++ Testing/test/muuvit-backend-tests.js
@@ -0,0 +1,66 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+require('dotenv').config();
+
+chai.use(chaiHttp);
+
+describe('POST Login tests', () => {
+    it('should return 200 status code', (done) => {
+        chai.request(process.env.BACKEND_URL)
+            .post('/auth/login')
+            .send({
+                userName: 'Backend_test',
+                password: 'Backend_test'
+            }).end((err, res) => {
+                chai.expect(res).to.have.status(200);
+                done();
+            });
+    });
+    it('should return a token', (done) => {
+        chai.request(process.env.BACKEND_URL)
+            .post('/auth/login')
+            .send({
+                userName: 'Backend_test',
+                password: 'Backend_test'
+            }).end((err, res) => {
+                console.log(res.data);
+                chai.expect(res.text).to.be.a('string').with.lengthOf.above(25);
+                done();
+            });
+    });
+    it('should return 401 status code', (done) => {
+        chai.request(process.env.BACKEND_URL)
+            .post('/auth/login')
+            .send({
+                userName: 'Backend_test',
+                password: 'wrong_password'
+            }).end((err, res) => {
+                chai.expect(res).to.have.status(401);
+                done();
+            });
+    });
+    it('should return "User not found"', (done) => {
+        chai.request(process.env.BACKEND_URL)
+            .post('/auth/login')
+            .send({
+                userName: 'wrong_user',
+                password: 'Backend_test'
+            }).end((err, res) => {
+                chai.expect(res.text).to.be.a('string').equal('User not found');
+                done();
+            });
+    });
+    it('should return "Invalid password"', (done) => {
+        chai.request(process.env.BACKEND_URL)
+            .post('/auth/login')
+            .send({
+                userName: 'Backend_test',
+                password: 'wrong_password'
+            }).end((err, res) => {
+                chai.expect(res.text).to.be.a('string').equal('Invalid password');
+                done();
+            });
+    });
+});
+
+module.exports = chai;
