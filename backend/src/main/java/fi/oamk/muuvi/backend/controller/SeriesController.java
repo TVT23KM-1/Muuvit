b6a15d4434721a7509badd575d24eb05f8069385
Mikko Kujala <m.kujala@live.com>
2024-04-08 08:46:44 +0300

    backend series search pelittää jotenkin

--- backend/src/main/java/fi/oamk/muuvi/backend/controller/SeriesController.java
+++ backend/src/main/java/fi/oamk/muuvi/backend/controller/SeriesController.java
@@ -1,20 +1,57 @@
 package fi.oamk.muuvi.backend.controller;
 
-import fi.oamk.muuvi.backend.Shemas.SeriesCategoriesSchema;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.TextNode;
+import fi.oamk.muuvi.backend.services.SeriesService;
+import io.swagger.v3.core.util.Json;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/series")
 public class SeriesController {
 
-    @GetMapping("/categories")
-    public ResponseEntity<List<SeriesCategoriesSchema>> getCategories() {
-        return null;
+    private SeriesService seriesService;
+
+    public SeriesController(SeriesService seriesService) {
+        this.seriesService = seriesService;
     }
+
+    @GetMapping("/categories")
+    public ResponseEntity<JsonNode> getCategories() {
+        try {
+            return ResponseEntity.ok(this.seriesService.getGenres());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<JsonNode> searchMovies(@RequestParam(required = false) String query,
+                                                    @RequestParam(required = false) String genre,
+                                                    @RequestParam(required = false) Integer page,
+                                                    @RequestParam(required = false) Integer year,
+                                                    @RequestParam(required = false) String language) throws IOException {
+        try {
+            return ResponseEntity.ok(seriesService.search(query, genre, page, year, language));
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+
 }
--- backend/src/main/java/fi/oamk/muuvi/backend/services/SeriesService.java
+++ backend/src/main/java/fi/oamk/muuvi/backend/services/SeriesService.java
@@ -1,14 +1,20 @@
 package fi.oamk.muuvi.backend.services;
 
-import fi.oamk.muuvi.backend.Shemas.SeriesCategoriesSchema;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.TextNode;
+import okhttp3.Call;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import org.springframework.beans.factory.annotation.Autowired;
+import okhttp3.Response;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class SeriesService {
@@ -22,24 +28,57 @@ public class SeriesService {
     }
 
 
-    public List<SeriesCategoriesSchema> getCategories() {
+    /**
+     * Get list of genres.
+     * @return
+     * @throws IOException
+     */
+    public JsonNode getGenres() throws IOException {
         Request request = new Request.Builder()
                 .url(String.format("https://api.themoviedb.org/3/genre/tv/list?api_key=%s&language=fi", api_key))
                 .get()
                 .build();
 
-        this.client.newCall(request).enqueue(new okhttp3.Callback() {
-            @Override
-            public void onFailure(okhttp3.Call call, IOException e) {
-                System.out.println("Error: " + e.getMessage());
-            }
+        try (Response resp = this.client.newCall(request).execute();)  {
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode result = mapper.readValue(resp.body().string(), JsonNode.class);
+            return result;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        }
+    }
 
-            @Override
-            public void onResponse(okhttp3.Call call, okhttp3.Response response) throws IOException {
-                System.out.println(response.body().string());
-            }
-        });
-        return null;
+    public JsonNode search(String queryString, String genreList, Integer page, Integer year, String language) throws IOException {
+        // Construct the URL based on the query parameters
+        String nameSearchString = queryString != null ? String.format("&query=%s", queryString) : "";
+        String genreSearch = genreList != null ? String.format("&with_genres=%s", genreList) : "";
+        String pageSearch = page != null ? String.format("&page=%s", page) : "";
+        String yearSearch = year != null ? String.format("&primary_release_year=%s", year) : "";
+        String languageSearch = language != null ? String.format("&language=%s", language) : "";
+
+        String URL;
+
+        // If the query string is null, we are using a different endpoint
+        if (queryString == null) {
+            URL = String.format("https://api.themoviedb.org/3/discover/tv?api_key=%s%s%s%s%s", this.api_key, genreSearch, pageSearch, yearSearch, languageSearch);
+        } else {
+            URL = String.format("https://api.themoviedb.org/3/search/tv?api_key=%s%s%s%s%s%s", this.api_key, nameSearchString, genreSearch, pageSearch, yearSearch, languageSearch);
+        }
+
+        // Execute the request and get the response body
+        Request req = new Request.Builder()
+                .url(URL)
+                .get()
+                .addHeader("accept", "application/json")
+                .build();
+        try (Response response = client.newCall(req).execute()) {
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode result = mapper.readValue(response.body().string(), JsonNode.class);
+            return result;
+        } catch (IOException e) {
+            throw e;
+        }
     }
 
 }
