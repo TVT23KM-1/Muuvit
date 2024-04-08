84b47496e041a3e9c8e9d01cb3f5aea2c84ad88b
Juho Hietala <juhohietala90@gmail.com>
2024-04-08 12:46:06 +0300

    Rekisterointi testit aloitettu. Testi kayttajan luomiseksi lapi

--- Testing/test/Login_tests.js
+++ Testing/test/Login_tests.js
@@ -5,18 +5,7 @@ require('dotenv').config();
 chai.use(chaiHttp);
 
 describe('POST Login tests', () => {
-    it('should return 200 status code', (done) => {
-        chai.request(process.env.BACKEND_URL)
-            .post('/auth/login')
-            .send({
-                userName: 'Backend_test',
-                password: 'Backend_test'
-            }).end((err, res) => {
-                chai.expect(res).to.have.status(200);
-                done();
-            });
-    });
-    it('should return a token', (done) => {
+    it('should return 200 status code and a token when succesfull', (done) => {
         chai.request(process.env.BACKEND_URL)
             .post('/auth/login')
             .send({
@@ -24,11 +13,12 @@ describe('POST Login tests', () => {
                 password: 'Backend_test'
             }).end((err, res) => {
                 console.log(res.data);
+                chai.expect(res).to.have.status(200);
                 chai.expect(res.text).to.be.a('string').with.lengthOf.above(25);
                 done();
             });
     });
-    it('should return 401 status code', (done) => {
+    it('should return 401 status code and a message "Invalid password" ', (done) => {
         chai.request(process.env.BACKEND_URL)
             .post('/auth/login')
             .send({
@@ -36,41 +26,31 @@ describe('POST Login tests', () => {
                 password: 'wrong_password'
             }).end((err, res) => {
                 chai.expect(res).to.have.status(401);
+                chai.expect(res.text).to.be.a('string').equal('Invalid password');
                 done();
             });
     });
-    it('should return "User not found"', (done) => {
+    it('should return 400 status code and a message "User not found"', (done) => {
         chai.request(process.env.BACKEND_URL)
             .post('/auth/login')
             .send({
                 userName: 'wrong_user',
                 password: 'Backend_test'
             }).end((err, res) => {
+                chai.expect(res).to.have.status(400);
                 chai.expect(res.text).to.be.a('string').equal('User not found');
                 done();
             });
     });
-    it('should return "Invalid password"', (done) => {
-        chai.request(process.env.BACKEND_URL)
-            .post('/auth/login')
-            .send({
-                userName: 'Backend_test',
-                password: 'wrong_password'
-            }).end((err, res) => {
-                chai.expect(res.text).to.be.a('string').equal('Invalid password');
-                done();
-            });
-    });
-    it('should return "Username or password missing"', (done) => {
+    it('should return 400 status code and a message "Username or password missing"', (done) => {
         chai.request(process.env.BACKEND_URL)
             .post('/auth/login')
             .send({
                 userName: 'Backend_test'
             }).end((err, res) => {
+                chai.expect(res).to.have.status(400);
                 chai.expect(res.text).to.be.a('string').equal('Username or password missing');
                 done();
             });
     });
 });
-
-module.exports = chai;
