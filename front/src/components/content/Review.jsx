d8f77848f7847f73ad56aa5ab2ae382859101289
taunojokinen <128710496+taunojokinen@users.noreply.github.com>
2024-04-09 13:19:38 +0300

    review aloitettu

--- front/src/components/content/Review.jsx
+++ front/src/components/content/Review.jsx
@@ -7,20 +7,43 @@ import '@content/css/Review.css'
 
 const Review = () => {
 
+    const sendReview = () => {
+        console.log('arvostelu')
+    }
+
     return (
         <>
         <div>
-        <h2>Puuttuuko tunnus?</h2>
+            <h2>Arvostele elokuva tai sarja</h2>
         </div>
-        <div id='review'>
-            <form>
+            <div id='review'>
+             <form id='reviewForm'>
+                
                 <label>Kirjoita arvostelu:
-                <input type="text" />
-      </label>
-    </form>
+                <input id="reviewText" placeholder="Kirjoita arvostelu tähän"></input>
+                </label>
+            </form>
 
 
         </div>
+
+        <div id="createReview">
+          <b>Uusi arvostelu</b> 
+
+          <b>Valitse elokuva:</b> (search) <br/>
+          <b>Anna tähdet: </b>
+          <select className="field" defaultValue="5">
+            <option value="1">&#11088; [1/5] tähteä</option>
+            <option value="2">&#11088;&#11088; [2/5] tähteä</option>
+            <option value="3">&#11088;&#11088;&#11088; [3/5] tähteä</option>
+            <option value="4">&#11088;&#11088;&#11088;&#11088; [4/5] tähteä</option>
+            <option value="5">&#11088;&#11088;&#11088;&#11088;&#11088; [5/5] tähteä</option>
+          </select>
+
+          <button onClick={sendReview}>Lähetä arvostelu</button>
+        </div>
+                
+
         </>
 
 
