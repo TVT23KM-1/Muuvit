778f7bc7a369d173ad33cf1b18cbd456bc98cc15
Juho Hietala <juhohietala90@gmail.com>
2024-04-09 12:30:00 +0300

    Add to favourites funkiot tehty, mutta testaamatta. Backendiin lisatty GenerateShareSlur luokka jonka staattinen metodi generoi user_id:n perusteella uniikin slurin. Toimii niin että jokaisella id:lla on eri slur

---
+++ front/src/components/content/AddFavourites.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { useLoginData} from '../../context/useLoginData';
+
+const addToFavourites = async (movie_id) => {
+  const loginData = useLoginData();
+  const token = loginData.token;
+  const request_body = {
+    movieId: movie_id
+  }
+  try {
+    const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/favourites/private/add`, request_body, Headers = { 'Authorization': `bearer ${token}` });
+    if (response.status === 200) {
+      return 'Kohde lisätty suosikkeihin';
+    } else if(response.status === 401) {
+      return 'Kirjaudu sisään lisätäksesi suosikkeihin';
+    } else {
+      return 'Virhe lisättäessä suosikkeihin';
+    }
+  } catch (error) {
+    console.error('Virhe lisättäessä suosikkeihin:', error);
+  }
+}
+
+export default addToFavourites;
