6b317518e61b79c2fd3fe18e7770bf3699fee420
Mikko Kujala <m.kujala@live.com>
2024-04-11 16:37:50 +0300

    callback for create groups component

--- front/src/components/content/CreateGroups.jsx
+++ front/src/components/content/CreateGroups.jsx
@@ -1,11 +1,17 @@
 import styles from '@content/css/CreateGroup.module.css';
 import '../../index.css'
-import React from "react";
+import React, {useEffect} from "react";
 import axios from 'axios';
 import {useState} from 'react';
 import {useLoginData} from '@context/useLoginData.jsx';
 
-const CreateGroups = () => {
+/**
+ * CreateGroups komponentti esittää formin jolla voi luoda uusia ryhmiä.
+ * @param onCreateGroup -funktio saa parametreinään ryhmän nimen ja kuvauksen. Funktiolla ei ole paluuarvoa.
+ * @returns {Element}
+ * @constructor
+ */
+const CreateGroups = (onCreateGroup) => {
 
     const loginData = useLoginData();
 
@@ -21,6 +27,7 @@ const CreateGroups = () => {
     }
     const [groupName, setGroupName] = useState('');
     const [groupDescription, setGroupDescription] = useState('');
+    const [triggerOnCreateGroup, setTriggerOnCreateGroup] = useState(false);
 
     const createGroup = (ev) => {
         return axios.post(`${import.meta.env.VITE_BACKEND_URL}/group/private/create`, {
@@ -33,6 +40,7 @@ const CreateGroups = () => {
                 'Authorization': `Bearer ${loginData.token}`
             }
         }).then((response) => {
+            setTriggerOnCreateGroup(true);
             return response;
         }).catch((error) => {
             console.log("token:", loginData.token);
@@ -41,32 +49,38 @@ const CreateGroups = () => {
         })
     }
 
+    useEffect(() => {
+        if (triggerOnCreateGroup && onCreateGroup) {
+            onCreateGroup(groupName, groupDescription);
+        }
+        setTriggerOnCreateGroup(false);
+    }, [triggerOnCreateGroup]);
 
     return (
         <>
             <h2>Ryhmän luominen</h2>
 
             {/* Uusien ryhmien rekisteröinti */}
-                <div className={styles.groupForm}>
-                    <label className={styles.groupNameLabel} htmlFor="group-name">Ryhmän nimi:</label>
-                    <input
-                        className={styles.groupNameInput}
-                        onChange={(ev) => setGroupName(ev.target.value)}
-                        value={groupName}
-                        type="text"
-                        id="group-name"
-                        name="group-name"
-                        required/>
-                    <label className={styles.groupDescriptionLabel} htmlFor="group-description">Kuvaus:</label>
-                    <textarea
-                        className={styles.groupDescriptionInput}
-                        onChange={(ev) => setGroupDescription(ev.target.value)}
-                        value={groupDescription}
-                        id="group-description"
-                        name="group-description"
-                        required/>
-                    <button type="button" onClick={createGroup}>Luo ryhmä</button>
-                </div>
+            <div className={styles.groupForm}>
+                <label className={styles.groupNameLabel} htmlFor="group-name">Ryhmän nimi:</label>
+                <input
+                    className={styles.groupNameInput}
+                    onChange={(ev) => setGroupName(ev.target.value)}
+                    value={groupName}
+                    type="text"
+                    id="group-name"
+                    name="group-name"
+                    required/>
+                <label className={styles.groupDescriptionLabel} htmlFor="group-description">Kuvaus:</label>
+                <textarea
+                    className={styles.groupDescriptionInput}
+                    onChange={(ev) => setGroupDescription(ev.target.value)}
+                    value={groupDescription}
+                    id="group-description"
+                    name="group-description"
+                    required/>
+                <button type="button" onClick={createGroup}>Luo ryhmä</button>
+            </div>
         </>
     )
 }
