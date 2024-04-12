ab00bae13e476d5c0429eac9d852904c6d54bc4e
Mikko Kujala <m.kujala@live.com>
2024-04-12 12:14:24 +0300

    Groups backend
    Get my groups toimii jos ei sitä yritä rikkoa.
    Get all groups toimii myös, jos muistaa että sitäkään ei saa rikkoa.

--- backend/src/main/java/fi/oamk/muuvi/backend/repositories/GroupRepository.java
+++ backend/src/main/java/fi/oamk/muuvi/backend/repositories/GroupRepository.java
@@ -13,7 +13,7 @@ public interface GroupRepository extends CrudRepository<Group, Long> {
     @Query(value="SELECT groups_.* FROM users JOIN userstogroups ON userstogroups.user_id = users.user_id JOIN groups_ ON groups_.group_id = userstogroups.group_id WHERE users.user_id = ?1", nativeQuery = true)
     List<Group> findMyGroups(Long userId);
 
-    @Query(value="SELECT g.group_id, g.group_name, g.group_description, utg.status, u.user_name FROM groups_ g JOIN userstogroups utg ON g.group_id = utg.group_id JOIN users u ON utg.user_id = u.user_id WHERE utg.status='owner'", nativeQuery = true)
+    @Query(value="SELECT g.* FROM groups_ g", nativeQuery = true)
     List<Group> findAllGroups();
     
 }
