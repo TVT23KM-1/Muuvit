0aeae441b5246b8dbf7a4bc7ae20bdd529478097
taunojokinen <128710496+taunojokinen@users.noreply.github.com>
2024-04-15 16:52:13 +0300

    metsästän

--- front/src/components/content/GroupPage.jsx
+++ front/src/components/content/GroupPage.jsx
@@ -4,29 +4,47 @@ import axios from "axios";
 import styles from "./css/GroupPage.module.css";
 
 const GroupPage = () => {
-    const [groupName, setGroupName] = useState();
     const [groupData, setGroupData] = useState();
+    const [members, setMembers] = useState();
     const {groupId} = useParams();
+    const renderMembers = () => {
+        console.log(groupData)
+        setMembers(
+        groupData?.participantRegistrations.map(participant  =>{
+            <li>{participant.user.username}</li>
+        }))
+    } 
 
     useEffect(() => {
         axios.get(`${import.meta.env.VITE_BACKEND_URL}/group/groupData/${groupId}`)
             .then(response => {
                 setGroupData(response.data)
-                setGroupName(response.data.groupName);
+                console.log("hello", response.data)
             })
             .catch(error => {
                 console.error('Virhe haettaessa ryhmää', error);
             })
-    })
+    },[])
+
+    useEffect(() => {
+        renderMembers()
+        console.log('hellomembers')
+    },[groupData])
 
     return (
         <>
-            <h1 className={styles.title}>Ryhmän <span>{groupName}</span> -sivu</h1>
+            <h1 className={styles.title}>Ryhmän <span>{groupData?.groupName}</span> -sivu</h1>
             <p className={styles.description}>{groupData?.groupDescription}</p>
             <hr className={styles.horizontalRuler}/>
             <div className={styles.sectioni}>
                 <h2>Ryhmän jäsenet</h2>
                 <button>Näytä</button>
+            </div>
+            <div>
+                <ul>
+                    {members}
+                    <li>Mikko</li>
+                </ul>
             </div>
             <hr className={styles.horizontalRuler}/>
             <div className={styles.sectioni}>
