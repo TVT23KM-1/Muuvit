05821db4eaa37f3315ed35cc12ac71eeffcd8628
taunojokinen <128710496+taunojokinen@users.noreply.github.com>
2024-04-19 09:57:12 +0300

    deleteaccount backend testattu ja toimii

--- backend/src/main/java/fi/oamk/muuvi/backend/controller/UserController.java
+++ backend/src/main/java/fi/oamk/muuvi/backend/controller/UserController.java
@@ -3,14 +3,18 @@ package fi.oamk.muuvi.backend.controller;
 
 import fi.oamk.muuvi.backend.Shemas.MovieResult;
 import fi.oamk.muuvi.backend.Shemas.UserInformation;
+import fi.oamk.muuvi.backend.repositories.UserRepository;
 import fi.oamk.muuvi.backend.services.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,15 +25,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private UserService userservice;
-    public UserController(UserService userservice){
+    private UserRepository userrepository;
+    public UserController(UserService userservice, UserRepository userrepository){
         this.userservice=userservice;
+        this.userrepository=userrepository;
     }
-    
-
 
     @PostMapping("/createAccount")
     public ResponseEntity<String> createAccount(@RequestBody UserInformation credentials) {
         return userservice.CreateAccount(credentials.getUserName(), credentials.getPassword());
+    }
+
+    @DeleteMapping("/private/deleteAccount")
+    public ResponseEntity<String> deleteAccount (@RequestAttribute(name = "jwtSub") Long userId) {
+        return userservice.deleteAccountByUserId(userId);
     }
     
 }
