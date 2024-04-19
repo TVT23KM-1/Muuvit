d5e1ac498f13165b5a82e66bec50965d7ad33f00
taunojokinen <128710496+taunojokinen@users.noreply.github.com>
2024-04-19 17:26:29 +0300

    hapuilua

--- front/src/pages/MyAccount.jsx
+++ front/src/pages/MyAccount.jsx
@@ -3,14 +3,50 @@ import { Navigate } from 'react-router-dom'
 import { useLoginData } from '../context/useLoginData';
 import ViewFavouritesList from '../components/content/ViewFavouritesList';
 import styles from './css/MyAccount.module.css';
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 
 export default function MyAccount () {
 
-  const onSelectDeleteAccount = (ev) => {
-    console.log('delete account')
-  }
+  const [deletGroupStatus, setDeleteGroupStatus] = useState({note: '',success: null, msg: 'Ei lähetty'})
 
   const loginData = useLoginData();
+
+  const onSelectDeleteAccount = (ev) => {
+    console.log('delete account')
+    ev.preventDefault()
+    axios({
+        url: `${import.meta.env.VITE_BACKEND_URL}/group/private/deleteGroup/${groupId}`,
+        method: 'delete',
+        withCredentials: true,
+        headers: {
+                allow: 'application/json',
+                "Authorization": `Bearer ${loginData.token}`
+        }
+    }).then(function (response) {
+        setDeleteGroupStatus({success: true, msg: 'Ryhmän poistaminen onnistui'})
+        navigate(-1);
+    }).catch(function (err ) {
+
+        if(err.message=="Network Error") {
+            console.log('haloo')
+            console.log(err.status)
+            console.log(err)
+            setDeleteGroupStatus({note: '\'poista ryhmä\'-viesti', success: false, msg: 'Ei yhteyttä tietokantaan'})
+        } else {
+            console.log(err)
+            console.log(err.status)
+            console.log(err.message)
+            setDeleteGroupStatus({note: '\'poista ryhmä\'-viesti', success: false, msg: 'tunnistamaton virhe'})
+        }
+
+    })
+
+
+  }
+
+  
     return (
       <div>
         <div className={styles.page}>
