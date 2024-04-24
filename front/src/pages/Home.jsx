bdd8ab59a881d3da20f195aaf03ca50fdbee5a31
taunojokinen <128710496+taunojokinen@users.noreply.github.com>
2024-04-24 16:52:11 +0300

    tuskailua jjiison kanssa

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import styles from'./css/Home.module.css';
 import Backdrop from "@content/Backdrop.jsx";
+import axios from 'axios';
+import { differenceInCalendarQuarters } from 'date-fns';
 
 /**
  * Home component is used to show the home page.
@@ -8,9 +10,83 @@ import Backdrop from "@content/Backdrop.jsx";
  */
 
 
+const tmdbImageFilePath = 'https://image.tmdb.org/t/p/w1280/';
+
+
 const Home = () => {
 
     const [showBackdrop, setShowBackdrop] = useState(true);
+    const [reviewStatus, setReviewStatus] = useState(true);
+    const [carouselleImage, setCarouselleImage] = useState('');
+    const [carouselleData, setCarouselleData] = useState([]); 
+    const [rndNum, setRndNum] = useState(0)
+
+
+    const [time, setTime] = useState(5);
+
+    useEffect(() => {
+      let timer = setInterval(() => {
+        setTime((time) => {
+          if (time === 0 ) {
+            //clearInterval(timer);
+            changeImage();
+            return 6;
+          } else {
+            return time-1
+          }
+        });
+      }, 1000);
+    },[]);
+  
+
+    const changeImage = () => {
+        setRndNum(randomNumberInRange(0, 19));
+        //console.log(carouselleData)
+        
+    }
+    const randomNumberInRange = (min, max) => {
+
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+
+
+
+
+
+    useEffect(() => {
+        getMovies()
+    },[])
+    
+        const getMovies = () => {
+
+            axios({
+                url: `${import.meta.env.VITE_BACKEND_URL}/movie/movieCarouselle/getMovies`,
+                method: 'get',
+            }).then(function (response) {
+                console.log(response.status)
+
+                setCarouselleImage(tmdbImageFilePath + response.data.results[0].backdrop_path)
+                console.log(response.data.results.length)
+                console.log(response.data.results)
+                const responseData = response.data.results
+                console.log(responseData)
+                console.log(responseData.length)
+                setCarouselleData(responseData.json)
+                console.log(carouselleData)
+            }).catch(function (err ) {
+                if (err.message=="Network Error") {
+                    console.log(err.status)
+                    console.log(err)
+                    setReviewStatus({success: false, msg: 'Ei yhteyttä tietokantaan'})
+                } else {
+                    setReviewStatus({success: false, msg: 'tunnistamaton virhe'})
+                }
+
+            })
+
+        }
+
+
 
     return (
         <div className = {styles.home}>
@@ -25,12 +101,10 @@ const Home = () => {
                 </p>
             </div>
             <div className={styles.poster}>
-                        <a href="https://www.themoviedb.org/" target="blank">
-                            <img src="https://ew.com/thmb/Z94jGmuVcHjbuUDzp89HBkRKe7U=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/mcdtogu_ec001_h-7da98a08ef124745b03e6ab97c7cccdd.jpg"
-                                 alt="TMDB logo"/>
-                        </a>
+                            <img src ={carouselleImage} alt='elokuvan posterikuva'></img>
                         <br/>
             </div>
+            <p> {time},{rndNum}</p>
         </div>
     )
 }
