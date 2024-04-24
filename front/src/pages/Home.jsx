38b34c9bbcbd6ff328d78a73eb5e321890f39df3
taunojokinen <128710496+taunojokinen@users.noreply.github.com>
2024-04-24 11:16:10 +0300

    karusellin front perustettu

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import './css/Home.css';
+import styles from'./css/Home.module.css';
 import Backdrop from "@content/Backdrop.jsx";
 
 /**
@@ -13,8 +13,8 @@ const Home = () => {
     const [showBackdrop, setShowBackdrop] = useState(true);
 
     return (
-        <>
-            <div className="page">
+        <div className = {styles.home}>
+            <div className={styles.page}>
                 <h2>Tervetuloa Muuvi -palveluun! </h2>
                 <p>
                     Kaipaatko paikkaa, jossa intohimo elokuviin ja sarjoihin yhdistää meidät kaikki? Muuvi on portti
@@ -24,8 +24,15 @@ const Home = () => {
                     kiinnostavat elokuvat ja sarjat.
                 </p>
             </div>
-        </>
-    );
-};
+            <div className={styles.poster}>
+                        <a href="https://www.themoviedb.org/" target="blank">
+                            <img src="https://ew.com/thmb/Z94jGmuVcHjbuUDzp89HBkRKe7U=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/mcdtogu_ec001_h-7da98a08ef124745b03e6ab97c7cccdd.jpg"
+                                 alt="TMDB logo"/>
+                        </a>
+                        <br/>
+            </div>
+        </div>
+    )
+}
 
 export default Home;
